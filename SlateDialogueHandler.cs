aab6733f0055d71a1a3077d86e0b9f760df3a0cd
SputnikSoyuz <bigfatsingularity@proton.me>
2026-05-26 21:42:16 -0400

    Tanks and Kitbashing
    - Adds fuel and oxygen tanks to the mod for refuel and oxygen volume purposes. You can find them on the islands of Blowing Behemoth.
    - Adds props and a recorder to the Frigid Fog Abandoned Campsite.
    - Added the White Hole device to the Anomaly Labs

--- SlateDialogueHandler.cs
+++ SlateDialogueHandler.cs
@@ -9,13 +9,12 @@ namespace NervesOfDarkness
 
         public void Start()
         {
-            //DialogueConditionManager.SharedInstance.SetConditionState("NoD_HornfelsIntro", false);
             slateDialogue = SearchUtilities.Find("TimberHearth_Body/Sector_TH/Sector_Village/Sector_StartingCamp/Characters_StartingCamp/Villager_HEA_Slate/NoD_SlateRemote");
         }
 
         public void Update()
         {
-            if (DialogueConditionManager.SharedInstance.GetConditionState("NoD_HornfelsIntro") && slateDialogue != null)
+            if (PlayerData.GetPersistentCondition("NoD_HornfelsIntro") && slateDialogue != null)
             {
                 Destroy(slateDialogue);
             }
